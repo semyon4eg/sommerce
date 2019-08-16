3dd65e4885b653bc551a25678581e23903d5b822
semyon4eg <semenowav@gmail.com>
2019-08-16 17:40:59 +0300

    Add orders module

--- config/web.php
+++ config/web.php
@@ -11,6 +11,11 @@ $config = [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+    'modules' => [
+        'orders' => [
+            'class' => 'app\modules\orders\Module',
+        ],
+    ],
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
